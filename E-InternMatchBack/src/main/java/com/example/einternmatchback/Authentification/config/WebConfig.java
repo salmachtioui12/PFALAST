bd6e0b6258fa4358fc933774f34905c27ed49940
salma <chtiouisalma790@gmail.com>
2025-05-13 17:41:17 +0100

    salma admin

---
+++ E-InternMatchBack/src/main/java/com/example/einternmatchback/Authentification/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.einternmatchback.Authentification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Configure Spring Boot pour servir des fichiers depuis le répertoire "uploads/company-logos"
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/company-logos/");
+    }
+}
